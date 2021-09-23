f895c8a7f6c481e0a6b2dccf1ed8a670468774a6
sainani3067 <ammu97938@gmail.com>
2021-09-23 17:15:03 +0530

    Uc1-uc8

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static Day4.Practise_Problems.UCLast_FinalSolution.cs.EmployeeWageBuilder;
+
+namespace Day4.Practise_Problems
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //UC1_EmployeeAttendance.GetAttendance();
+             //UC2_EmpWage.EmpWage();
+            //UC3_PartTimeEmpWage.PartTimeEmpWage();
+            //UC4_EmpWageUsingSwitch.EmpWageUsingSwitch();
+            //UC5_CalculateWagesFor20DaysInMonth.CalculateWagesFor20DaysInMonth();
+            //UC6_CalculateWageTill100HrsOr20IsReached.CalculateWageTill100HrsOr20IsReached();
+            //UC7_ComputeEmpWageUsingClassMethod.ComputeEmpWage();
+            //UC8_ComputeEmpWageForMultipleCompanies.ComputeEmpWage("abc", 4, 5, 7);
+
+            
+            Console.ReadKey();
+
+        }
+    }
+}
+
+
